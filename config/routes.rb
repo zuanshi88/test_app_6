8a5cbcb284cf03d251cc0c654b30781cddc5d335
zuanshi88 <adwhitmer@gmail.com>
2022-05-07 11:43:43 -0500

    DRY initial CRUD functionality and Restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   resources :posts 
+
+  # could restrict with a hash of only: [show, edit]
+  #expose all REST-ful routes-- representational state transfer
+  # basically a mapping of the HTTP verbs(get, post, put/patch, delete)
+  # to CRUD actions in Rails App.
+  #HTTP verbs front end requests -- CRUD actions backend responses.
+  #frontend could be anything just as long as the HTTP verbs are being
+  #translated into the appropriate/correct CRUD action.
+
 end
