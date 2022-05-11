4705a56288dc88dda471660bf9ae0caa50a52f29
zuanshi88 <adwhitmer@gmail.com>
2022-05-11 08:25:27 -0500

    created user migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_160810) do
+ActiveRecord::Schema.define(version: 2022_05_11_131453) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2022_01_14_160810) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
