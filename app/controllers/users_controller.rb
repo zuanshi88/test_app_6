33982d7cfb4455f01954d8a413a12eee44d8d65a
zuanshi88 <adwhitmer@gmail.com>
2022-05-11 16:00:42 -0500

    homepage sign up button operational!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save 
-            flash[:notice] = "Welcome to the Alpha Blog-- signup success!"
+            flash[:notice] = "Welcome to the Alpha Blog, #{@user.username}-- signup success!"
             redirect_to articles_path 
         else  
             render 'new'
