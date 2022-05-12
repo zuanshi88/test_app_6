8f272143a8e572a33af965c1d03c690643f1f671
zuanshi88 <adwhitmer@gmail.com>
2022-05-12 11:58:33 -0500

    added pagination with will_paginate gem

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'bcrypt', '~> 3.1.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+gem 'will_paginate', '~> 3.1.0'
+
 group :development, :test do
   gem 'sqlite3', '~> 1.4'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
     
     def show 
         @user = User.find(params[:id])
-        @articles = @user.articles.reverse
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end 
+
+    def index 
+        @users = User.paginate(page: params[:page], per_page: 5).order('id DESC')
     end 
 
     def new 
@@ -17,7 +21,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information was successfully updated"
-            redirect_to articles_path
+            redirect_to @user
         else  
             render 'edit'
         end 
@@ -27,10 +31,16 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save 
             flash[:notice] = "Welcome to the Alpha Blog, #{@user.username}-- signup success!"
-            redirect_to articles_path 
+            redirect_to @user 
         else  
             render 'new'
         end 
+    end 
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.delete
+        redirect_to users_path
     end 
 
 
