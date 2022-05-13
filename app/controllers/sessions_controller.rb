fd705bc29315c74784d92d3627ed145c0cb025e6
zuanshi88 <adwhitmer@gmail.com>
2022-05-13 11:57:53 -0500

    deploying to Heroku

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
               session[:user_id] = user.id
-              flash[:alert] = "Logged in successfully"
+              flash[:notice] = "Logged in successfully"
               redirect_to user 
         else 
             flash.now[:alert] = "There was something wrong with your login details."
