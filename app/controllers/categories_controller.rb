f61546eff94a4c47113c295f384926477bff9820
zuanshi88 <adwhitmer@gmail.com>
2022-05-17 17:38:30 -0500

    categories edit implemented

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,8 +20,23 @@ class CategoriesController < ApplicationController
         end 
     end 
 
+    def edit 
+        @category = Category.find(params[:id])
+    end 
+
+    def update 
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:notice] = "Category name updated successfully"
+            redirect_to @category 
+        else  
+            render 'edit'
+        end 
+    end 
+
     def show 
         @category = Category.find(params[:id])
+        @articles = @category.articles.paginate(page: params[:page], per_page: 5)
     end 
 
     private 
