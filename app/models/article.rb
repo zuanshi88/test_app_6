2c7f185e46e084bceda1d3d2b30d8073ae3b4300
zuanshi88 <adwhitmer@gmail.com>
2022-10-05 15:15:25 -0500

    add index_content method to Article model to build search functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,20 @@ class Article < ApplicationRecord
     has_many :categories, through: :article_categories
     validates :title, presence: true, length: { minimum: 6, maximum: 100}
     validates :description, presence: true, length: { minimum: 10, maximum: 10000}
+
+
+     def self.index_content
+            # information = ["name", "last_name", "first_name"]
+            content_hash = {}
+            self.all.each do |article|
+                unless article.description == nil || note.comment == ""
+                    article.description.split(/[\s,'-\.!]/).each do |word|
+                            content_hash[word.downcase] = [] if content_hash[word.downcase].nil? 
+                            content_hash[word.downcase].push(article))
+                    end 
+                end 
+            end 
+        content_hash
+    end
+
 end
